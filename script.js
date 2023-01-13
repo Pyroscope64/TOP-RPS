0e6e7085a14fc0dc68c9c74a26ec2eff4c94a78c
Pyroscope64 <amethystbeatz@gmail.com>
2023-01-13 18:33:45 +0000

    Made code more readable

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let compScore = 0, playerScore = 0;
-function Random(max) {
-    return Math.floor(Math.random() * max);
-}
 function getComputerChoice() {
-    let random = Random(3);
+    let random = Math.floor(Math.random() * 3);
     if (random === 0) return "rock";
     else if (random === 1) return "paper";
     else if (random === 2) return "scissors";
@@ -15,44 +12,39 @@ function getPlayerChoice() {
     } while (choice != "rock" && choice != "paper" && choice != "scissors");
     return choice;
 }
-
+function getWinner(playerChoice, compChoice) {
+    if (playerChoice === "rock" && compChoice === "scissors") { 
+        return "Player wins!"; 
+    }
+    else if (playerChoice === "rock" && compChoice === "paper") { 
+        return "Computer wins!"; 
+    }
+    else if (playerChoice === "paper" && compChoice === "rock") { 
+        return "Player wins!";
+    }
+    else if (playerChoice === "paper" && compChoice === "scissors") { 
+        return "Computer wins!"; 
+    }
+    else if (playerChoice === "scissors" && compChoice === "paper") { 
+        return "Player wins!"; 
+    }
+    else if (playerChoice === "scissors" && compChoice === "rock") { 
+        return "Computer wins!"; 
+    }
+    else if (playerChoice === compChoice) return "Draw!!!";
+}
+function Play(playerChoice, compChoice) {
+    document.getElementById("displaytext").innerText = 
+    (`Player chose ${playerChoice} and Computer chose 
+    ${compChoice}\n${winner}`);
+}
 function Game() {
     let playerChoice = getPlayerChoice();
     let compChoice = getComputerChoice();
     let winner = getWinner(playerChoice, compChoice);
-
     if (winner === "Player wins!") playerScore++;
     else if (winner === "Computer wins!") compScore++;
     document.getElementById("button").innerHTML =
     (`${playerScore} | ${compScore}`);
-
-    function Play(playerChoice, compChoice) {
-        document.getElementById("displaytext").innerText = 
-        (`Player chose ${playerChoice} and Computer chose ${compChoice}\n
-          ${winner}`);
-    }
-
-    function getWinner(playerChoice, compChoice) {
-        if (playerChoice === "rock" && compChoice === "scissors") { 
-            return "Player wins!"; 
-        }
-        else if (playerChoice === "rock" && compChoice === "paper") { 
-            return "Computer wins!"; 
-        }
-        else if (playerChoice === "paper" && compChoice === "rock") { 
-            return "Player wins!";
-        }
-        else if (playerChoice === "paper" && compChoice === "scissors") { 
-            return "Computer wins!"; 
-        }
-        else if (playerChoice === "scissors" && compChoice === "paper") { 
-            return "Player wins!"; 
-        }
-        else if (playerChoice === "scissors" && compChoice === "rock") { 
-            return "Computer wins!"; 
-        }
-        else if (playerChoice === compChoice) return "Draw!!!";
-    }
-
     Play(playerChoice, compChoice);
 }
